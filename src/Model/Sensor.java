dd871234440b157731c5b32ad5ef0126f20f8479
David Guasca <davidsantiagoguasca@gmail.com>
2023-03-09 08:27:10 -0500

    Sensor y Alarma

---
+++ src/Model/Sensor.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Sensor {
+
+    private int id;
+
+    public Sensor() {
+
+    }
+
+    public Sensor(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
