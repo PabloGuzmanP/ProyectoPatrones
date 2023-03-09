31c96e525db0a5ae50bb3cbbfd9b0e4d3eb9780c
juanpagu2101 <guzmanpjpablo@gmail.com>
2023-03-09 08:33:26 -0500

    Actualizar clase alarma

---
+++ src/Model/Alarma.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Alarma {
+    private int id;
+
+    public Alarma() {
+
+    }
+    public Alarma(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
